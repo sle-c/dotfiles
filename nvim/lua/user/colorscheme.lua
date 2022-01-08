e8d19626cbc82654ae07a664d7d6023b670f5100
Si Le <omnisyle@gmail.com>
2022-01-08 00:28:32 -0500

    Fixes tmux color

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -3,11 +3,6 @@ local colorscheme = "tokyonight"
 if colorscheme == "tokyonight" then
   -- make it darker
   vim.g.tokyonight_style = "night"
-
-  -- customize some color
-  vim.g.tokyonight_colors = {
-    bg_visual = "bg_highlight"
-  }
 end
 
 local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
