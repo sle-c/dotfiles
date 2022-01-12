138e685e862edcc04f822d4a89a55b9db1d24b97
Si Le <omnisyle@gmail.com>
2022-01-11 20:18:47 -0500

    Changes indentline to not show space dots

--- nvim/lua/user/indentline.lua
+++ nvim/lua/user/indentline.lua
@@ -15,17 +15,17 @@ vim.wo.colorcolumn = "99999"
 -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
 
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "space:⋅"
 -- vim.opt.listchars:append "space:"
 vim.opt.listchars:append "eol:↴"
 
 indent_blankline.setup({
   use_treesitter = true,
   blankline_char = "▏",
-	show_end_of_line = true,
-	space_char_blankline = " ",
-	show_current_context = true,
-	show_current_context_start = true,
+  show_end_of_line = true,
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
   show_trailing_blankline_indent = false,
   show_first_indent_level = false,
   buftype_exclude = { "terminal", "nofile" },
